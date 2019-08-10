3ae5d92fee162c5a5f296dc525f5a62672e80a81
Jonathan Wong <jonathan.wong@applydigital.co>
2019-08-10 16:12:59 -0400

    Abstracted out task fetching to custom hook logic

--- client/src/components/EditTask.js
+++ client/src/components/EditTask.js
@@ -1,29 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TaskForm from "./TaskForm";
-import axios from "axios";
-import { useTaskState } from "./../context/task-context";
 
+import useFetchTask from "./../hooks/useFetchTask";
 export default function EditTask({ match }) {
-  const [task, setTask] = useState();
-  const { tasks } = useTaskState();
-
-  useEffect(() => {
-    const editedTask = tasks.find(task => task._id === match.params.id);
-    const getTask = async () => {
-      const response = await axios({
-        method: "GET",
-        url: `http://localhost:3000/api/v1/tasks/${match.params.id}`,
-        withCredentials: true
-      });
-      setTask(response.data.data.task);
-    };
-    if (editedTask) {
-      setTask(editedTask);
-    } else {
-      getTask();
-    }
-  }, [tasks, match.params.id]);
-
+  const task = useFetchTask(match.params.id);
   if (!task) return <div />;
   return (
     <div>
---
+++ client/src/hooks/useFetchTask.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import { useTaskState } from "./../context/task-context";
+import axios from "axios";
+
+function useFetchTask(id) {
+  const [fetchedTask, setFetchedTask] = useState();
+  const { tasks } = useTaskState();
+
+  useEffect(() => {
+    const task = tasks.find(task => task._id === id);
+    const getTask = async () => {
+      const response = await axios({
+        method: "GET",
+        url: `http://localhost:3000/api/v1/tasks/${id}`,
+        withCredentials: true
+      });
+      setFetchedTask(response.data.data.task);
+    };
+    if (task) {
+      setFetchedTask(task);
+    } else {
+      getTask();
+    }
+  }, [tasks, id]);
+
+  return fetchedTask;
+}
+
+export default useFetchTask;
