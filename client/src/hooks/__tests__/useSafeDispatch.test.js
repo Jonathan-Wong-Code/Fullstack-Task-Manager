2ffa8ac6cb0cfa1591a2ceaf0cc896b0fceb8797
Jonathan Wong <jonathan.wong@applydigital.co>
2019-08-19 13:52:38 -0400

    Tried another hook test

--- client/src/hooks/__tests__/useSafeDispatch.test.js
+++ client/src/hooks/__tests__/useSafeDispatch.test.js
@@ -1,15 +1,33 @@
-import { renderHook } from "@testing-library/react-hooks";
+import React, { useEffect } from "react";
+import { renderContext } from "./../../testUtils/testUtils";
 import useSafeDispatch from "./../useSafeDispatch";
-jest.mock("./../useSafeDispatch");
+import { wait, cleanup } from "@testing-library/react";
+afterEach(cleanup);
 
-const safeSetState = jest.fn();
+function TestComponent() {
+  const [{ user, email }, safeSetState] = useSafeDispatch({
+    user: "",
+    email: ""
+  });
 
-useSafeDispatch.mockImplementation(() => [
-  { state: "test state" },
-  safeSetState
-]);
+  useEffect(() => {
+    setTimeout(() => {
+      safeSetState({ user: "test-user", email: "test-email" });
+    }, 2000);
+  }, [safeSetState]);
 
-test("should return the state and setSafeSet", () => {
-  const { result } = renderHook(() => useSafeDispatch());
-  expect(result.current).toEqual([{ state: "test state" }, safeSetState]);
+  return (
+    <>
+      <p data-testid="user">{user}</p>
+      <p data-testid="email">{email}</p>
+    </>
+  );
+}
+
+test("should return state and setSafeState", async () => {
+  const { getByTestId } = renderContext(<TestComponent />, { route: "/" });
+  await wait(() => {
+    expect(getByTestId("user").textContent).toBe("test-user");
+    expect(getByTestId("email").textContent).toBe("test-email");
+  });
 });
